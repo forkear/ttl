47a8fa1a729176606e6cb9611202c68b11fa1689
Forkear <forkear@no-spam-github.com>
2023-08-20 00:41:25 -0300

    20/08/2023-00:41:25:499361300

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "98e5bb26d7d81f6d66fcd18caa4836c7",
-"/": "98e5bb26d7d81f6d66fcd18caa4836c7",
+const RESOURCES = {"index.html": "ae2fedb3ba51680f1337e2732c870912",
+"/": "ae2fedb3ba51680f1337e2732c870912",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
-"main.dart.js": "67566dfb36840c0a165e8cf4883f6fbc",
+"main.dart.js": "ec5fc2fede8d78ed9ea7c4f7e26e19f6",
 "favicon.png": "5280cb02c15ae6f3f94786c1ed3e0f2e",
 "icons/512x512.png": "581604136adf3345aebaeaa92ed7f826",
 "icons/192x192.png": "bc143240b44f331f08989cc8cc36f818",
