9a04c330ff5a816d7323a5c2c7aa1f4db29f3ce0
Forkear <forkear@no-spam-github.com>
2024-04-21 12:48:04 -0300

    21/04/2024-12:48:04:248038229

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"main.dart.js": "2bff7b07055c8a121f584957b2b2f801",
+const RESOURCES = {"main.dart.js": "b0d961f39ceb08fc55e156bc5db2b312",
 "favicon.png": "5280cb02c15ae6f3f94786c1ed3e0f2e",
 "icons/512x512.png": "581604136adf3345aebaeaa92ed7f826",
 "icons/192x192.png": "bc143240b44f331f08989cc8cc36f818",
@@ -21,8 +21,8 @@ const RESOURCES = {"main.dart.js": "2bff7b07055c8a121f584957b2b2f801",
 "canvaskit/canvaskit.wasm": "3d2a2d663e8c5111ac61a46367f751ac",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
-"index.html": "d6c2b91a83d93a19766873f1bdcb7e21",
-"/": "d6c2b91a83d93a19766873f1bdcb7e21",
+"index.html": "1915d3dbeb779466d82edba110926f20",
+"/": "1915d3dbeb779466d82edba110926f20",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "b93248a553f9e8bc17f1065929d5934b",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
 "assets/lib/src/assets/images/48x48.png": "5280cb02c15ae6f3f94786c1ed3e0f2e",
